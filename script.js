0b2b418fe46edcfb51c5fb81aa9e2fdbd23877d6
Josep Tarruella <joseptg@hotmail.com>
2025-04-16 17:44:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 fetch("maraton_barcelona_2025_graph.json")
-  .then((res) => {
-    console.log("Respuesta del fetch:", res);
-    return res.json();
-  })
+  .then((res) => res.json())
   .then((data) => {
-    console.log("Datos cargados del JSON:", data);
-    // (resto del código...)
-  })
-  .catch((err) => {
-    console.error("Error al cargar el JSON:", err);
-  });
-
-    // Posicionar los nodos
-    data.nodes.forEach((n) => {
-      const latlng = L.latLng(n.data.latitude, n.data.longitude);
-      const point = map.latLngToContainerPoint(latlng);
-      cy.getElementById(n.data.id).position({ x: point.x, y: point.y });
+    const cy = cytoscape({
+      container: document.getElementById("cy"),
+      elements: [...data.nodes, ...data.edges],
+      style: [
+        {
+          selector: "node",
+          style: {
+            "background-color": "#0074D9",
+            "label": "data(label)",
+            "width": 12,
+            "height": 12
+          }
+        },
+        {
+          selector: "edge",
+          style: {
+            "line-color": "#AAAAAA",
+            "width": 3
+          }
+        }
+      ],
+      layout: { name: "preset" }
     });
-
     // Interacción
     cy.on("tap", "node", (evt) => {
       const d = evt.target.data();
